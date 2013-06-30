9d2ac448dd7a7bd8afc229a1dcaf30b38e3569a0
Hyosup Shim <flourscent@gmail.com>
2013-06-30 12:33:58 +0900

    wrote Fill.asm

--- chap04/fill/Fill.asm
+++ chap04/fill/Fill.asm
@@ -8,4 +8,101 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+	@WD_CNT
+	@CURR_PIXEL
+	@SCREEN
+	D=A
+	@FIRST_PIXEL
+	M=D
+	// bit pattern white
+	@PIXEL_PTN_WHT
+	D=0
+	M=D
+	// bit pattern black
+	@PIXEL_PTN_BLK
+	D=0
+	D=!D
+	M=D
+
+(KEYWAIT_LOOP)
+	// check whether key pressed
+	@KBD
+	D=M
+	@BLACKEN
+	D;JNE
+	@WHITEN
+	D;JEQ
+	@KEYWAIT_LOOP
+	0;JMP
+
+(BLACKEN)
+	@8191
+	D=A
+	@WD_CNT
+	M=D
+
+	@SCREEN
+	D=A
+	@CURPIXEL
+	M=D
+(BLACKEN_LOOP)
+	@WD_CNT
+	D=M
+	@SCREEN
+	A=D+A
+	D=A
+	@CURR_PIXEL
+	M=D
+
+	@PIXEL_PTN_BLK
+	D=M
+
+	@CURR_PIXEL
+	A=M
+	M=D
+
+	@WD_CNT
+	D=M
+	D=D-1
+	M=D
+
+	@KEYWAIT_LOOP
+	D;JLT
+	@BLACKEN_LOOP
+	0;JMP
+
+(WHITEN)
+	@8191
+	D=A
+	@WD_CNT
+	M=D
+	
+	@SCREEN
+	D=A
+	@CURPIXEL
+	M=D
+(WHITEN_LOOP)
+	@WD_CNT
+	D=M
+	@SCREEN
+	A=D+A
+	D=A
+	@CURR_PIXEL
+	M=D
+
+	@PIXEL_PTN_WHT
+	D=M
+
+	@CURR_PIXEL
+	A=M
+	M=D
+
+	@WD_CNT
+	D=M
+	D=D-1
+	M=D
+
+	@KEYWAIT_LOOP
+	D;JLT
+	@WHITEN_LOOP
+	0;JMP
